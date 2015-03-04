3229ee87fdd04238e4aafcde02e7dade22dd6827
markeban <markeban@yahoo.com>
2015-03-04 07:57:18 -0600

    added City of Chicago API data to table

--- js/misc_ctrl.js
+++ js/misc_ctrl.js
@@ -1,7 +1,7 @@
 (function () {
   "use strict";
 
-  angular.module("app").controller("miscCtrl", function($scope) {
+  angular.module("app").controller("miscCtrl", function($scope, $http) {
 
     $scope.message = "Hello World!";
 
@@ -16,6 +16,10 @@
       var index = $scope.tasks.indexOf(task);
       $scope.tasks.splice(index, 1);
     }
+
+    $http.get("https://data.cityofchicago.org/resource/xzkq-xp2w.json")
+    .success(function(response) {$scope.cityWorkers = response;
+    });
 
   });
 })();
